8d282c682177dd6484111bd903ef62681f930d39
papeixoto <pazevedopeixoto@gmail.com>
2024-05-10 23:26:30 +0100

    backend authentication

--- convex/files.ts
+++ convex/files.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const createFile = mutation({
@@ -6,6 +6,12 @@ export const createFile = mutation({
     name: v.string(),
   },
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("You must be signed in to create a file");
+    }
+
     await ctx.db.insert("files", {
       name: args.name,
     });
@@ -15,6 +21,12 @@ export const createFile = mutation({
 export const getFiles = query({
   args: {},
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      return [];
+    }
+
     return await ctx.db.query("files").collect();
   },
 });
