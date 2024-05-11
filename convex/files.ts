492238348f713b7692130096bcfc8d3460b49197
papeixoto <pazevedopeixoto@gmail.com>
2024-05-11 22:19:14 +0100

    add hability to delete files

--- convex/files.ts
+++ convex/files.ts
@@ -85,3 +85,38 @@ export const getFiles = query({
       .collect();
   },
 });
+
+export const deleteFile = mutation({
+  args: {
+    fileId: v.id("files"),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError(
+        "You don't have permission to delete a file in this organization"
+      );
+    }
+
+    const file = await ctx.db.get(args.fileId);
+
+    if (!file) {
+      throw new ConvexError("File not found");
+    }
+
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      file.orgId
+    );
+
+    if (!hasAccess) {
+      throw new ConvexError(
+        "You don't have permission to delete a file in this organization"
+      );
+    }
+
+    await ctx.db.delete(args.fileId);
+  },
+});
--- src/app/file-card.tsx
+++ src/app/file-card.tsx
@@ -1,23 +1,102 @@
+import { useState } from "react";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Doc } from "../../convex/_generated/dataModel";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { MoreVertical, TrashIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { useToast } from "@/components/ui/use-toast";
 
 type Props = {
   file: Doc<"files">;
 };
 
+function FileCardActions({ file }: Props) {
+  const { toast } = useToast();
+  const deleteFile = useMutation(api.files.deleteFile);
+  const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState(false);
+
+  return (
+    <>
+      <AlertDialog
+        open={isConfirmDeleteOpen}
+        onOpenChange={setIsConfirmDeleteOpen}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async () => {
+                await deleteFile({
+                  fileId: file._id,
+                });
+                toast({
+                  title: "File deleted",
+                  description: "Your file is now gone from the system.",
+                });
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MoreVertical />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className="flex gap-1 text-red-600 items-center cursor-pointer"
+            onClick={() => setIsConfirmDeleteOpen(true)}
+          >
+            <TrashIcon className="w-4 h-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
 export function FileCard({ file }: Props) {
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="relative">
         <CardTitle>{file.name}</CardTitle>
+
+        <div className="absolute top-2 right-2">
+          <FileCardActions file={file} />
+        </div>
         {/* <CardDescription>Card Description</CardDescription> */}
       </CardHeader>
       <CardContent>
