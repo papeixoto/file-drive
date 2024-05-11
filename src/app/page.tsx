8ca74cb4fa4a57989acc4f26d827ab7aa4769eb6
papeixoto <pazevedopeixoto@gmail.com>
2024-05-11 18:09:38 +0100

    modal to upload file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,54 @@
 "use client";
 
-import { useOrganization, useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQuery } from "convex/react";
+import { useForm } from "react-hook-form";
 import { api } from "../../convex/_generated/api";
+
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, "Required"),
+});
 
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+  const fileRef = form.register("file");
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
 
   let orgId: string | undefined;
   if (organization.isLoaded && user.isLoaded) {
@@ -18,19 +59,68 @@ export default function Home() {
   const createFile = useMutation(api.files.createFile);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container mx-auto pt-12">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold">Your Files</h1>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              onClick={() => {
+                if (!orgId) return;
+                createFile({ name: "test", orgId });
+              }}
+            >
+              Upload File
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-8">Upload your file here</DialogTitle>
+              <DialogDescription>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Title</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={() => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...fileRef} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className=""></div>
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>;
       })}
-
-      <Button
-        onClick={() => {
-          if (!orgId) return;
-          createFile({ name: "test", orgId });
-        }}
-      >
-        click me
-      </Button>
     </main>
   );
 }
